a949c02b0b5ef1a8c9a7ef87679147d520b8b627
timcatrino99 <timcatrino99@gmail.com>
2025-07-28 19:52:18 -0400

    get_book_text added

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
